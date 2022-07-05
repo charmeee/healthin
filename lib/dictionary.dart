5c96b81ec2295f6de69a5fa6b76be13eef8e4714
charmeee <ppseoja@pusan.ac.kr>
2022-07-06 05:15:49 +0900

    draftappver1

---
+++ lib/dictionary.dart
@@ -0,0 +1,205 @@
+import 'dart:developer';
+import 'dart:io';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+final List<String> healthtype = [
+  "가슴",
+  "등",
+  "어깨",
+  "팔",
+  "복근",
+  "하체",
+  "유산소",
+];
+
+class Exercise {
+  int? id;
+  String? name;
+  String? enname;
+  String? type;
+  int? difficulty;
+  List? content;
+  List? precautions;
+  // "id": ,
+  // "name": "",
+  // "enName": "",
+  // "type": "",
+  // "difficulty": 1,
+  // "content": [
+  // "",
+  // ],
+  // "Precautions": ["",]
+
+  Exercise(
+      {this.id,
+      this.name,
+      this.enname,
+      this.type,
+      this.difficulty,
+      this.content,
+      this.precautions});
+  Exercise.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        enname = json['enname'],
+        type = json['type'],
+        difficulty = json['difficulty'],
+        content = json['content'],
+        precautions = json['precautions'];
+}
+
+class Dictionary extends StatefulWidget {
+  Dictionary({Key? key}) : super(key: key);
+
+  @override
+  State<Dictionary> createState() => _DictionaryState();
+}
+
+class _DictionaryState extends State<Dictionary> {
+  var isSelected = List<bool>.filled(healthtype.length, false);
+  FocusNode focusNode = FocusNode();
+  String _searchText = "";
+  List<Exercise>? alldata = []; //json 파일받아온값
+  List<Exercise>? founddata = []; //찾은데이터=> 출력데이터
+  List<Exercise>? nonchipselecteddata = []; //칩 셀랙전 데이터
+
+  Future<void> readJson() async {
+    //json파일 읽어오기
+    final String response =
+        await rootBundle.loadString('testjsonfile/healthmachinedata.json');
+    //print(response.runtimeType);
+    Map<String, dynamic> _alldata = await jsonDecode(response);
+    setState(() {
+      alldata = [..._alldata["items"].map((item) => Exercise.fromJson(item))];
+      founddata = [...alldata!];
+    });
+  }
+
+  void initState() {
+    super.initState();
+    readJson();
+  }
+
+  void runSearchFilter(String enteredKeyword) {
+    List<Exercise>? result = [];
+    if (enteredKeyword.isEmpty) {
+      result = [...alldata!];
+    } else {
+      result = alldata!
+          .where((data) =>
+              data.name!.toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      founddata = result;
+    });
+  }
+
+  void runChipFilter() {
+    List<Exercise>? result = [];
+    bool flag = true;
+    for (int i = 0; i < healthtype.length; i++) {
+      if (isSelected[i] == true) {
+        flag = false;
+        result = alldata!
+            .where((data) => data.type!.contains(healthtype[i]))
+            .toList();
+      }
+    }
+    if (flag) {
+      result = [...alldata!];
+    }
+    setState(() {
+      founddata = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+            height: 40,
+            child: TextField(
+              style: TextStyle(fontSize: 15),
+              focusNode: focusNode,
+              autofocus: false,
+              keyboardType: TextInputType.text,
+              onChanged: (text) => runSearchFilter(text),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                hintText: "운동을 검색해주세요",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 1, color: Colors.indigo),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 3, color: Colors.indigo),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: healthtype.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 3, 10, 3),
+                    child: ChoiceChip(
+                      backgroundColor: Colors.indigo[50],
+                      label: Container(
+                          alignment: Alignment.center,
+                          width: 40,
+                          height: 30,
+                          child: Text(healthtype[index])),
+                      selected: isSelected[index],
+                      onSelected: (bool value) {
+                        setState(() {
+                          isSelected =
+                              List<bool>.filled(healthtype.length, false);
+                          isSelected[index] = value;
+                          runChipFilter();
+                        });
+                      },
+                      labelStyle: TextStyle(
+                          color:
+                              isSelected[index] ? Colors.white : Colors.indigo),
+                      selectedColor: Colors.indigo,
+                    ),
+                  );
+                }),
+          ),
+          Expanded(
+            child: Container(
+              child: alldata == null || alldata!.isEmpty
+                  ? Text("로딩중")
+                  : ListView.separated(
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(founddata![index].name.toString()),
+                        );
+                      },
+                      itemCount: founddata!.length,
+                      separatorBuilder: (BuildContext context, int index) =>
+                          Divider(
+                        height: 10,
+                        color: Colors.indigo,
+                      ),
+                    ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
